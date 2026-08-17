5dae7c39b53a371ce6209fadfa97a806bf3f33d9
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2026-08-17 10:44:26 +0200

    Initial commit

--- src/styles_provider/__init__.py
+++ src/styles_provider/__init__.py
@@ -1,3 +1,1 @@
-from .base_style import BaseStyleProvider
-from .file_system_style import FileSystemStyleProvider
-from .http_style import HttpStyleProvider
+from .style import StyleProvider
--- src/styles_provider/style.py
+++ src/styles_provider/style.py
@@ -1,6 +1,14 @@
-from typing import Dict, List, Any
+from urllib.parse import urljoin, urlparse
+from urllib.request import url2pathname
+from pathlib import Path
+import logging
+from typing import Any, Dict, List
+import requests
+from .uri_type import UriType
 
-STYLESHEET_TYPES = {
+_logger = logging.getLogger(__name__)
+
+_stylesheet_types = {
     'mapbox': {
         'title': 'Mapbox Style',
         'mime_type': 'application/vnd.mapbox.style+json',
@@ -12,7 +20,7 @@ STYLESHEET_TYPES = {
         'title': 'OGC SE',
         'mime_type': 'application/vnd.ogc.se+xml;version=1.1',
         'version': '1.1',
-        'spec': 'https://www.ogc.org/standards/se',        
+        'spec': 'https://www.ogc.org/standards/se',
         'format': 'se11'
     },
     'sld10': {
@@ -25,13 +33,14 @@ STYLESHEET_TYPES = {
 }
 
 
-class BaseStyleProvider:
+class StyleProvider:
     def __init__(self, provider_def: Dict):
         self.styles: List[Dict[str, Any]] = provider_def['styles']
-        self.server_url = provider_def['server_url']
+        self.server_url: str = provider_def['server_url']
+        self.base_uri: str | None = provider_def.get('base_uri')
 
     def get_styles(self) -> Dict[str, Any]:
-        styles: List[Dict] = []
+        styles: List[Dict[str, Any]] = []
 
         for style in self.styles:
             id: str = style['id']
@@ -41,12 +50,6 @@ class BaseStyleProvider:
                     'rel': 'describedby',
                     'title': 'Style metadata',
                     'href': f'{self.server_url}/styles/{id}/metadata'
-                },
-                {
-                    'rel': 'stylesheet',
-                    'type': 'text/html',
-                    'title': 'Web map using the style',
-                    'href': f'{self.server_url}/styles/{id}?f=html'
                 }
             ]
 
@@ -57,9 +60,9 @@ class BaseStyleProvider:
 
                 links.append({
                     'rel': 'stylesheet',
-                    'type': STYLESHEET_TYPES[type]['mime_type'],
-                    'title': f'Style in {STYLESHEET_TYPES[type]["title"]} format',
-                    'href': f'{self.server_url}/styles/{id}?f={STYLESHEET_TYPES[type]["format"]}'
+                    'type': _stylesheet_types[type]['mime_type'],
+                    'title': f'Style in {_stylesheet_types[type]["title"]} format',
+                    'href': f'{self.server_url}/styles/{id}?f={_stylesheet_types[type]["format"]}'
                 })
 
             styles.append({
@@ -78,14 +81,14 @@ class BaseStyleProvider:
             (style for style in styles if style['id'] == style_id), None)
 
         return style
-    
+
     def get_style_metadata(self, style_id: str) -> Dict[str, Any] | None:
         style = next(
             (style for style in self.styles if style['id'] == style_id), None)
 
         if not style:
             return None
-        
+
         metadata = {
             'id': style['id'],
             'title': style['title']
@@ -100,7 +103,7 @@ class BaseStyleProvider:
 
         if keywords:
             metadata['keywords'] = keywords
-            
+
         metadata['scope'] = 'style'
 
         version = style.get('version')
@@ -108,12 +111,12 @@ class BaseStyleProvider:
         if version:
             metadata['version'] = version
 
-        stylesheets: List[Dict] = style['stylesheets']
+        stylesheets: List[Dict[str, Any]] = style['stylesheets']
         metadata['stylesheets'] = []
 
         for styleheet in stylesheets:
-            type = STYLESHEET_TYPES[styleheet['type']]
-            
+            type = _stylesheet_types[styleheet['type']]
+
             metadata['stylesheets'].append({
                 'title': type['title'],
                 'version': type['version'],
@@ -126,19 +129,82 @@ class BaseStyleProvider:
                 }
             })
 
-        layers: List[Dict] = style['layers']
+        layers: List[Dict[str, Any]] = style['layers']
         metadata['layers'] = []
 
-        for layer in layers:           
+        for layer in layers:
             metadata['layers'].append({
                 'id': layer['id'],
                 'type': layer['type']
             })
 
-        return metadata  
+        return metadata
 
     def get_style_definition(self, style_id: str, format_: str) -> str | None:
-        raise NotImplementedError()
+        style = next(
+            (style for style in self.styles if style['id'] == style_id), None)
+
+        if not style:
+            return None
+
+        stylesheets: List[Dict[str, Any]] = style.get('stylesheets', [])
+        stylesheet = next(
+            (stylesheet for stylesheet in stylesheets if stylesheet['type'] == format_), None)
+
+        if not stylesheet:
+            return None
+
+        uri: str = stylesheet['uri']
+
+        uri_type = self._get_uri_type(
+            self.base_uri) if self.base_uri else self._get_uri_type(uri)
+
+        if uri_type == UriType.HTTP_URL:
+            return self._get_style_from_http(self.base_uri, uri)
+
+        base_uri = self.base_uri
+
+        if uri_type == UriType.FILE_URL:
+            if self.base_uri:
+                parsed_url = urlparse(self.base_uri)
+                base_uri = url2pathname(parsed_url.path)
+            else:
+                parsed_url = urlparse(uri)
+                uri = url2pathname(parsed_url.path)
+
+        return self._get_style_from_path(base_uri, uri)
 
     def get_style_preview(self, style_id: str):
         raise NotImplementedError()
+
+    def _get_style_from_path(self, base_uri: str | None, uri: str) -> str | None:
+        path = Path(base_uri).joinpath(uri) if base_uri else Path(uri)
+
+        if not path.exists():
+            return None
+
+        with open(path, 'r') as file:
+            return file.read()
+
+    def _get_style_from_http(self, base_uri: str | None, uri: str) -> str | None:
+        url = urljoin(base_uri.rstrip('/') + '/', uri) if base_uri else uri
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+
+            return response.text
+        except Exception as err:
+            _logger.warning(f'Could not fetch style definition: {url}', err)
+            return None
+
+    def _get_uri_type(self, uri: str) -> UriType:
+        parsed = urlparse(uri)
+
+        if parsed.scheme in ('http', 'https'):
+            return UriType.HTTP_URL
+
+        if parsed.scheme == 'file':
+            return UriType.FILE_URL
+
+        return UriType.PATH
